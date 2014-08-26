48b5f1b7f61438d8059a09eaa30a9234fae04bf9
Rich Luick <rluick15@gmail.com>
2014-08-26 11:10:58 -0400

    Add logging out capabilities

--- app/src/main/java/com/richluick/ribbit/MainActivity.java
+++ app/src/main/java/com/richluick/ribbit/MainActivity.java
@@ -50,10 +50,7 @@ public class MainActivity extends Activity implements ActionBar.TabListener {
 
         ParseUser currentUser = ParseUser.getCurrentUser();
         if(currentUser == null) {
-            Intent intent = new Intent(this, LoginActivity.class);
-            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
-            startActivity(intent);
+            navigateToLogin();
         }
         else {
             Log.i(TAG, currentUser.getUsername());
@@ -94,6 +91,13 @@ public class MainActivity extends Activity implements ActionBar.TabListener {
         }
     }
 
+    private void navigateToLogin() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -104,12 +108,10 @@ public class MainActivity extends Activity implements ActionBar.TabListener {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+        if (id == R.id.action_logout) {
+            ParseUser.logOut();
+            navigateToLogin();
         }
         return super.onOptionsItemSelected(item);
     }
