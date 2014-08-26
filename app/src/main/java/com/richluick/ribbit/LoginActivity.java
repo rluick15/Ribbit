176d23c488c70f3172f70f56234a18635fbe1288
Rich Luick <rluick15@gmail.com>
2014-08-26 10:51:57 -0400

    Add Logging in capabilities

--- app/src/main/java/com/richluick/ribbit/LoginActivity.java
+++ app/src/main/java/com/richluick/ribbit/LoginActivity.java
@@ -1,13 +1,24 @@
 package com.richluick.ribbit;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+
 public class LoginActivity extends Activity {
+
+    protected EditText mUsername;
+    protected EditText mPassword;
+    protected Button mLoginButton;
 
     protected TextView mSignUpTextView;
 
@@ -22,6 +33,51 @@ public class LoginActivity extends Activity {
            public void onClick(View view) {
                Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
                startActivity(intent);
+           }
+       });
+
+       mUsername = (EditText) findViewById(R.id.usernameField);
+       mPassword = (EditText) findViewById(R.id.passwordField);
+       mLoginButton = (Button) findViewById(R.id.loginButton);
+
+       mLoginButton.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               String username = mUsername.getText().toString();
+               String password = mPassword.getText().toString();
+
+               username = username.trim();
+               password = password.trim();
+
+               if (username.isEmpty() || password.isEmpty()) {
+                   AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+                   builder.setTitle(getString(R.string.login_error_title))
+                           .setMessage(getString(R.string.login_error_message))
+                           .setPositiveButton(android.R.string.ok, null);
+                   AlertDialog dialog = builder.create();
+                   dialog.show();
+               }
+               else {
+                   ParseUser.logInInBackground(username, password, new LogInCallback() {
+                       @Override
+                       public void done(ParseUser parseUser, ParseException e) {
+                           if (e == null) {
+                               Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                               intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                               intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                               startActivity(intent);
+                           }
+                           else {
+                               AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+                               builder.setTitle(R.string.login_error_title)
+                                       .setMessage(e.getMessage())
+                                       .setPositiveButton(android.R.string.ok, null);
+                               AlertDialog dialog = builder.create();
+                               dialog.show();
+                           }
+                       }
+                   });
+               }
            }
        });
     }
--- app/src/main/java/com/richluick/ribbit/MainActivity.java
+++ app/src/main/java/com/richluick/ribbit/MainActivity.java
@@ -9,6 +9,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v13.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -16,11 +17,14 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.parse.ParseAnalytics;
+import com.parse.ParseUser;
 
 import java.util.Locale;
 
 
 public class MainActivity extends Activity implements ActionBar.TabListener {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
 
     /**
      * The {@link android.support.v4.view.PagerAdapter} that will provide
@@ -44,10 +48,16 @@ public class MainActivity extends Activity implements ActionBar.TabListener {
 
         ParseAnalytics.trackAppOpened(getIntent());
 
-        Intent intent = new Intent(this, LoginActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        startActivity(intent);
+        ParseUser currentUser = ParseUser.getCurrentUser();
+        if(currentUser == null) {
+            Intent intent = new Intent(this, LoginActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+        }
+        else {
+            Log.i(TAG, currentUser.getUsername());
+        }
 
         // Set up the action bar.
         final ActionBar actionBar = getActionBar();
