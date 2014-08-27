20fdd8e189c72fe17ff5fffca744b0fa185eef32
Rich Luick <rluick15@gmail.com>
2014-08-27 14:05:48 -0400

    add ability to create friend relations

--- app/src/main/java/com/richluick/ribbit/EditFriendsActivity.java
+++ app/src/main/java/com/richluick/ribbit/EditFriendsActivity.java
@@ -1,19 +1,86 @@
 package com.richluick.ribbit;
 
-import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.widget.AbsListView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseRelation;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+import java.util.List;
 
 
-public class EditFriendsActivity extends Activity {
+public class EditFriendsActivity extends ListActivity {
+
+    public static final String TAG = EditFriendsActivity.class.getSimpleName();
+
+    protected List<ParseUser> mUsers;
+    protected ParseRelation<ParseUser> mFriendsRelation;
+    protected ParseUser mCurrentUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
         setContentView(R.layout.activity_edit_friends);
+
+        getListView().setChoiceMode(AbsListView.CHOICE_MODE_MULTIPLE);
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        mCurrentUser = ParseUser.getCurrentUser();
+        mFriendsRelation = mCurrentUser.getRelation(ParseConstants.KEY_FRIENDS_RELATION);
+
+        setProgressBarIndeterminateVisibility(true);
+
+        ParseQuery<ParseUser> query = ParseUser.getQuery();
+        query.orderByAscending(ParseConstants.KEY_UNSERNAME);
+        query.setLimit(1000);
+        query.findInBackground(new FindCallback<ParseUser>() {
+            @Override
+            public void done(List<ParseUser> parseUsers, ParseException e) {
+                setProgressBarIndeterminateVisibility(false);
+
+                if(e == null) {
+                    mUsers = parseUsers;
+                    String[] usernames = new String[mUsers.size()];
+                    int i = 0;
+                    for(ParseUser user : mUsers) {
+                        usernames[i] = user.getUsername();
+                        i++;
+                    }
+                    ArrayAdapter<String> adapter = new ArrayAdapter<String>(EditFriendsActivity.this,
+                            android.R.layout.simple_list_item_checked,
+                            usernames);
+                    setListAdapter(adapter);
+                }
+                else {
+                    Log.e(TAG, e.getMessage());
+                    AlertDialog.Builder builder = new AlertDialog.Builder(EditFriendsActivity.this);
+                    builder.setTitle(R.string.error_title)
+                            .setMessage(e.getMessage())
+                            .setPositiveButton(android.R.string.ok, null);
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                }
+            }
+        });
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -32,5 +99,25 @@ public class EditFriendsActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+
+        if (getListView().isItemChecked(position)) {
+            mFriendsRelation.add(mUsers.get(position));
+            mCurrentUser.saveInBackground(new SaveCallback() {
+                @Override
+                public void done(ParseException e) {
+                    if (e != null) {
+                        Log.e(TAG, e.getMessage());
+                    }
+                }
+            });
+        }
+        else {
+            
+        }
     }
 }
--- app/src/main/java/com/richluick/ribbit/ParseConstants.java
+++ app/src/main/java/com/richluick/ribbit/ParseConstants.java
@@ -3,5 +3,10 @@ package com.richluick.ribbit;
 /**
  * Created by Rich on 8/27/2014.
  */
-public class ParseConstants {
+public final class ParseConstants {
+    //Class name
+
+    //Field names
+    public static final String KEY_UNSERNAME = "username";
+    public static final String KEY_FRIENDS_RELATION = "friendsRelation";
 }
