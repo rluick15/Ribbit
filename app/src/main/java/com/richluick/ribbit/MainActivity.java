7ed2b30d25d9dba8eb204ad5b653fb888087f07b
Rich Luick <rluick15@gmail.com>
2014-09-04 11:12:11 -0400

    set file path of image taken by camera

--- app/src/main/java/com/richluick/ribbit/MainActivity.java
+++ app/src/main/java/com/richluick/ribbit/MainActivity.java
@@ -5,28 +5,48 @@ import android.app.AlertDialog;
 import android.app.FragmentTransaction;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v13.app.FragmentPagerAdapter;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.parse.ParseAnalytics;
 import com.parse.ParseUser;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 
 public class MainActivity extends FragmentActivity implements ActionBar.TabListener {
 
     public static final String TAG = MainActivity.class.getSimpleName();
 
+    public static final int TAKE_PHOTO_REQUEST = 0;
+    public static final int TAKE_VIDEO_REQUEST = 1;
+    public static final int PICK_PHOTO_REQUEST = 2;
+    public static final int PICK_VIDEO_REQUEST = 3;
+
+    public static final int MEDIA_TYPE_IMAGE = 4;
+    public static final int MEDIA_TYPE_VIDEO = 5;
+
+    protected Uri mMediaUri;
+
     protected DialogInterface.OnClickListener mDialogListener = new DialogInterface.OnClickListener() {
         @Override
         public void onClick(DialogInterface dialogInterface, int i) {
             switch(i) {
-                case 0:
-                    break; //take pic
+                case 0: //Take a Picture
+                    takePicture();
+                    break;
                 case 1:
                     break; //take vid
                 case 2:
@@ -35,6 +55,75 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
                     break; //choose vid
             }
 
+        }
+
+        private void takePicture() {
+            Intent takePhotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            mMediaUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE);
+            if (mMediaUri == null) {
+                //display an error
+                Toast.makeText(MainActivity.this, getString(R.string.error_external_storage), Toast.LENGTH_LONG).show();
+            }
+            else {
+                takePhotoIntent.putExtra(MediaStore.EXTRA_OUTPUT, mMediaUri);
+                startActivityForResult(takePhotoIntent, TAKE_PHOTO_REQUEST);
+            }
+        }
+
+        private Uri getOutputMediaFileUri(int mediaType) {
+            // To be safe, you should check that the SDCard is mounted
+            // using Environment.getExternalStorageState() before doing this.
+
+            if (isExternalStorageAvailable()) { //get the Uri
+                //Get external storage directory
+                String appName = MainActivity.this.getString(R.string.app_name);
+                File mediaStorageDir = new File(
+                        Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
+                        appName);
+
+                //create subdirectory
+                if (! mediaStorageDir.exists()) {
+                   if (! mediaStorageDir.mkdirs()) {
+                        Log.e(TAG, getString(R.string.failed_to_create_dir_log_message));
+                        return null;
+                    }
+                }
+
+                //Create File name and file
+                File mediaFile;
+                Date now = new Date();
+                String timestamp = new SimpleDateFormat("yyyMMdd_HHmmss", Locale.US).format(now);
+
+                String path = mediaStorageDir.getPath() + File.separator;
+                if(mediaType == MEDIA_TYPE_IMAGE) {
+                    mediaFile = new File(path + "IMG_" + timestamp + ".jpg");
+                }
+                else if(mediaType == MEDIA_TYPE_VIDEO) {
+                    mediaFile = new File(path + "VID_" + timestamp + ".mp4");
+                }
+                else {
+                    return null;
+                }
+
+                Log.d(TAG, "File: " + Uri.fromFile(mediaFile));
+
+                //return files Uri
+                return Uri.fromFile(mediaFile);
+            }
+            else {
+                return null;
+            }
+        }
+
+        private boolean isExternalStorageAvailable() {
+            String state = Environment.getExternalStorageState();
+
+            if (state.equals(Environment.MEDIA_MOUNTED)) {
+                return true;
+            }
+            else {
+                return false;
+            }
         }
     };
 
